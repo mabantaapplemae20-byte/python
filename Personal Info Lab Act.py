e940d9e7fffeeb19804db1fdae1dec3c4bab06f9
mabantaapplemae20-byte <mabanta.applemae20@gmail.com>
2026-02-12 23:30:32 +0800

    Add files via upload
    Laboratory Activity about Variables

---
+++ Personal Info Lab Act.py
@@ -0,0 +1,19 @@
+#Personal Information
+name="Apple Mae Mabanta"
+print("Fullname: ", name)
+print(type(name))
+print ()
+
+age=20
+print("Age: ", age)
+print(type(age))
+print ()
+
+height=1.56
+print("Height in meter: ", height)
+print(type(height))
+print ()
+
+working=True
+print("Currently working?: ", working)
+print(type(working))
